b064f541f40da953cb3fa150852468b75cc61b49
angular-moon <xiaoxuss@163.com>
2018-12-27 19:36:37 +0800

    remove Counter

--- packages/demo-components/src/App.tsx
+++ packages/demo-components/src/App.tsx
@@ -1,46 +1,46 @@
 import { hot, setConfig } from 'react-hot-loader';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { LocaleProvider, Modal } from 'antd';
-import { Router, Route, Prompt, Link } from 'react-router-dom';
+import { LocaleProvider } from 'antd';
+import { Router } from 'react-router-dom';
 import { utils } from 'demo-common';
 import zhCN from 'antd/es/locale-provider/zh_CN';
-import { TodoType, OperationType } from 'demo-common/src/enums';
+import { OperationType } from 'demo-common/src/enums';
 import { OpinionStrategy } from 'demo-common/src/types/Operation';
 import ApplyView from './components/ApplyView';
 import ApplyForm from './components/ApplyForm';
 import { Mode } from './components/ApplyForm/enums/Mode';
-import Counter from './components/Counter/Counter';
-import { getConfirmation } from './utils/getConfirmation';
 
 const App = () => (
   <LocaleProvider locale={zhCN}>
     <Provider store={utils.stateContainer._store}>
       <Router history={utils.stateContainer._history}>
-        <Link to="/Form">Form</Link>
-        <Route
-          path="/"
-          render={() => (
-            <ApplyForm
-              processInstanceId="1"
-              taskId="1"
-              mode={Mode.CREATE}
-              operations={[
-                {
-                  name: '提交',
-                  type: OperationType.SUBMIT,
-                  selectKey: '1',
-                  opinionStrategy: OpinionStrategy.OPTIONAL,
-                },
-                {
-                  name: '退回',
-                  type: OperationType.REJECT,
-                  selectKey: '1',
-                  opinionStrategy: OpinionStrategy.OPTIONAL,
-                },
-              ]}
-            />
-          )}
+        <ApplyView
+          applyId="1"
+          processInstanceId="1"
+          taskId="1"
+          operations={[
+            {
+              name: '提交',
+              type: OperationType.SUBMIT,
+              selectKey: '1',
+              opinionStrategy: OpinionStrategy.OPTIONAL,
+            },
+          ]}
+        />
+        <ApplyForm
+          applyId="1"
+          processInstanceId="1"
+          taskId="1"
+          mode={Mode.CREATE}
+          operations={[
+            {
+              name: '提交',
+              type: OperationType.SUBMIT,
+              selectKey: '1',
+              opinionStrategy: OpinionStrategy.OPTIONAL,
+            },
+          ]}
         />
       </Router>
     </Provider>
