39c140bc9e1496c8a4dceff706eddac15bf49427
angular-moon <xiaoxuss@163.com>
2019-01-21 10:43:01 +0800

    remove dozc temp file

--- packages/demo-components/.docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'src/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'),
-  'src/components/Alert/Alert.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-alert-alert" */ 'src/components/Alert/Alert.mdx'),
-  'src/components/ApplyView/ApplyView.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-apply-view-apply-view" */ 'src/components/ApplyView/ApplyView.mdx'),
-}
